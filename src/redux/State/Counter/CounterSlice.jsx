5c07d96bf758e4f6890cc66151ef29ba78001395
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-10-09 12:49:37 +0600

    CounterSlice is created

---
+++ src/redux/State/Counter/CounterSlice.jsx
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CounterSlice = createSlice({
+    name:"Counter",
+    initialState:{
+        value:0
+    },
+
+    reducers:{
+        increment:(state)=>{
+            state.value = state.value + 1;
+        }, 
+        decrement:(state)=>{
+            state.value = state.value - 1;
+        }, 
+    }
+})
