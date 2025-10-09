fa7d3a3772c48125034e66f61aa8d5d45646aa92
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-10-09 11:56:57 +0600

    Create Store component and add to the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
+import { Provider } from 'react-redux';
+import Store from './redux/Store/Store.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
